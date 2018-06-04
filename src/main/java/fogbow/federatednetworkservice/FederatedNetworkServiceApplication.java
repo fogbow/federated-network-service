72f8b921723ae5d991285594a83abd8ba8f375eb
Gustavo Yamaguchi <gustavoyama@gmail.com>
2018-06-04 13:52:28 -0300

    Init project

---
+++ src/main/java/fogbow/federatednetworkservice/FederatedNetworkServiceApplication.java
@@ -0,0 +1,12 @@
+package fogbow.federatednetworkservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FederatedNetworkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FederatedNetworkServiceApplication.class, args);
+	}
+}
