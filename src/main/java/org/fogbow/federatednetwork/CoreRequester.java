0fe92777ef925091f35008dc407362572bb80025
Gustavo Yamaguchi <gustavoyama@gmail.com>
2018-06-05 10:43:03 -0300

    Change core requester api signature

--- src/main/java/org/fogbow/federatednetwork/CoreRequester.java
+++ src/main/java/org/fogbow/federatednetwork/CoreRequester.java
@@ -1,15 +1,15 @@
 package org.fogbow.federatednetwork;
 
-
-import org.fogbow.federatednetwork.model.Order;
+import org.fogbowcloud.manager.core.models.orders.ComputeOrder;
+import org.fogbowcloud.manager.core.models.instances.ComputeInstance;
 
 public class CoreRequester {
 
-	public boolean createCompute(Order order){
+	public boolean createCompute(ComputeOrder order){
 		throw new UnsupportedOperationException();
 	}
 
-	public Order getCompute(String id){
+	public ComputeInstance getCompute(String orderId){
 		throw new UnsupportedOperationException();
 	}
 
