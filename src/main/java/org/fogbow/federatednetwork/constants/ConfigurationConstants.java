cbeaab6faef7d404a9f6321bdfbb41e893c16836
fubica <fubica@dsc.ufcg.edu.br>
2018-09-20 17:15:04 -0300

    Implemented version endpoint and created constants.Messages

---
+++ src/main/java/org/fogbow/federatednetwork/constants/ConfigurationConstants.java
@@ -0,0 +1,18 @@
+package org.fogbow.federatednetwork.constants;
+
+public class ConfigurationConstants {
+
+    // Manager Core Properties
+    public static final String RAS_PORT = "ras_port";
+    public static final String RAS_IP = "ras_ip";
+    public static final String RAS_NAME = "ras_name";
+
+    // Federated Network Properties
+    public static final String FEDERATED_NETWORK_AGENT_PERMISSION_FILE_PATH = "federated_network_agent_permission_file_path";
+    public static final String FEDERATED_NETWORK_AGENT_USER = "federated_network_agent_user";
+    public static final String FEDERATED_NETWORK_AGENT_PRIVATE_ADDRESS = "federated_network_agent_private_address";
+    public static final String FEDERATED_NETWORK_AGENT_ADDRESS = "federated_network_agent_address";
+    public static final String FEDERATED_NETWORK_PRE_SHARED_KEY = "federated_network_agent_pre_shared_key";
+    public static final String ADD_FEDERATED_NETWORK_SCRIPT_PATH = "add_federated_network_script_path";
+    public static final String REMOVE_FEDERATED_NETWORK_SCRIPT_PATH = "remove_federated_network_script_path";
+}
