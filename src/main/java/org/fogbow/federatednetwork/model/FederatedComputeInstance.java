42e6a08de5ac13d0f0a04d9676bad990c4a4ead4
Gustavo Yamaguchi <gustavoyama@gmail.com>
2018-10-02 11:45:26 -0300

    Fixing Federated compute instance to match RAS compute instance

--- src/main/java/org/fogbow/federatednetwork/model/FederatedComputeInstance.java
+++ src/main/java/org/fogbow/federatednetwork/model/FederatedComputeInstance.java
@@ -3,20 +3,22 @@ package org.fogbow.federatednetwork.model;
 import org.fogbowcloud.ras.core.models.instances.ComputeInstance;
 import org.fogbowcloud.ras.core.models.instances.InstanceState;
 
+import java.util.List;
+
 public class FederatedComputeInstance extends ComputeInstance {
 
     private String federatedIp;
 
     public FederatedComputeInstance(String id, String hostName, int vCPU, int memory, InstanceState state,
-                                    String localIpAddress, int disk, String federatedIp) {
+                                    List<String> ipAddresses, int disk, String federatedIp) {
 
-        super(id, state, hostName, vCPU, memory, disk, localIpAddress);
+        super(id, state, hostName, vCPU, memory, disk, ipAddresses);
         this.federatedIp = federatedIp;
     }
 
     public FederatedComputeInstance(ComputeInstance computeInstance, String federatedIp) {
         this(computeInstance.getId(), computeInstance.getHostName(), computeInstance.getvCPU(),
-                computeInstance.getRam(), computeInstance.getState(), computeInstance.getLocalIpAddress(),
+                computeInstance.getRam(), computeInstance.getState(), computeInstance.getIpAddresses(),
                 computeInstance.getDisk(), federatedIp);
     }
 
