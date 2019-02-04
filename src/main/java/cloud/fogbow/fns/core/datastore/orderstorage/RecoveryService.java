73576a0f02b341b4dc188577c721930de7493363
Gustavo Yamaguchi <gustavoyama@gmail.com>
2019-02-04 14:33:29 -0300

    Adding log when there's no ips avaliable

--- src/main/java/cloud/fogbow/fns/core/datastore/orderstorage/RecoveryService.java
+++ src/main/java/cloud/fogbow/fns/core/datastore/orderstorage/RecoveryService.java
@@ -1,10 +1,12 @@
 package cloud.fogbow.fns.core.datastore.orderstorage;
 
+import cloud.fogbow.fns.core.constants.Messages;
 import cloud.fogbow.fns.core.exceptions.InvalidCidrException;
 import cloud.fogbow.fns.core.exceptions.SubnetAddressesCapacityReachedException;
 import cloud.fogbow.fns.core.model.FederatedNetworkOrder;
 import cloud.fogbow.fns.core.model.OrderState;
 import cloud.fogbow.fns.utils.FederatedNetworkUtil;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,8 @@ public class RecoveryService {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    private static final Logger LOGGER = Logger.getLogger(RecoveryService.class);
 
     public FederatedNetworkOrder put(FederatedNetworkOrder order) {
         return orderRepository.save(order);
@@ -32,7 +36,7 @@ public class RecoveryService {
             try {
                 FederatedNetworkUtil.fillCacheOfFreeIps((FederatedNetworkOrder) order);
             } catch (SubnetAddressesCapacityReachedException e) {
-                // TODO put logging
+                LOGGER.info(Messages.Exception.NO_MORE_IPS_AVAILABLE);
             } catch (InvalidCidrException e) {
             }
             activeOrdersMap.put(order.getId(), order);
