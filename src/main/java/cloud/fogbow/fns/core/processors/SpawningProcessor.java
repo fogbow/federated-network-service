568472800c973f87106f17fd3ed205c6fb765ea1
Paulo Ewerton <pauloewerton@lsd.ufcg.edu.br>
2019-04-04 16:58:38 -0300

    Adding spawning processor state transitions

--- src/main/java/cloud/fogbow/fns/core/processors/SpawningProcessor.java
+++ src/main/java/cloud/fogbow/fns/core/processors/SpawningProcessor.java
@@ -2,14 +2,31 @@ package cloud.fogbow.fns.core.processors;
 
 import cloud.fogbow.common.exceptions.UnexpectedException;
 import cloud.fogbow.common.models.linkedlists.ChainedList;
+import cloud.fogbow.fns.constants.ApiDocumentation;
 import cloud.fogbow.fns.constants.Messages;
 import cloud.fogbow.fns.core.FederatedNetworkOrderController;
 import cloud.fogbow.fns.core.FederatedNetworkOrdersHolder;
+import cloud.fogbow.fns.core.OrderStateTransitioner;
 import cloud.fogbow.fns.core.exceptions.AgentCommucationException;
+import cloud.fogbow.fns.core.exceptions.InvalidCidrException;
+import cloud.fogbow.fns.core.model.ConfigurationMode;
 import cloud.fogbow.fns.core.model.FederatedNetworkOrder;
+import cloud.fogbow.fns.core.model.MemberConfigurationState;
 import cloud.fogbow.fns.core.model.OrderState;
+import cloud.fogbow.fns.core.serviceconnector.ServiceConnector;
+import cloud.fogbow.fns.core.serviceconnector.ServiceConnectorFactory;
 import cloud.fogbow.fns.utils.AgentCommunicatorUtil;
+import cloud.fogbow.fns.utils.FederatedNetworkUtil;
+import cloud.fogbow.ras.core.models.orders.Order;
+import org.apache.commons.net.util.SubnetUtils;
 import org.apache.log4j.Logger;
+import org.aspectj.weaver.ast.Or;
+
+import javax.xml.ws.Service;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SpawningProcessor implements Runnable {
     private static final Logger LOGGER = Logger.getLogger(cloud.fogbow.ras.core.processors.ClosedProcessor.class);
@@ -48,8 +65,65 @@ public class SpawningProcessor implements Runnable {
         synchronized (order) {
             // Check if the order is still SPAWNING (its state may have been changed by another thread)
             if (order.getOrderState().equals(OrderState.SPAWNING)) {
-                this.orderController.deactivateOrder(order);
+                ConfigurationMode configurationMode = order.getConfigurationMode();
+                ServiceConnector connector = ServiceConnectorFactory.getInstance().getServiceConnector(
+                        order.getConfigurationMode(), order.getRequestingMember());
+
+                switch(configurationMode) {
+                    case VANILLA:
+                        MemberConfigurationState state = connector.configure(order);
+                        if (state == MemberConfigurationState.SUCCESS) {
+                            OrderStateTransitioner.transition(order, OrderState.FULFILLED);
+                        } else {
+                            OrderStateTransitioner.transition(order, OrderState.FAILED);
+                        }
+                        break;
+                    case DFNS:
+                        this.createDFNSFederatedNetwork(order);
+                    default:
+                        throw new RuntimeException(Messages.Exception.CONFIGURATION_MODE_NOT_IMPLEMENTED);
+                }
+            }
+
+
+        }
+    }
+
+    private void createDFNSFederatedNetwork(FederatedNetworkOrder order) throws UnexpectedException {
+        List<MemberConfigurationState> memberConfigurationStates = new ArrayList<>();
+        for (Map.Entry<String, MemberConfigurationState> provider : order.getProviders().entrySet()) {
+            ServiceConnector connector = ServiceConnectorFactory.getInstance().getServiceConnector(
+                    ConfigurationMode.DFNS, provider.getKey());
+            MemberConfigurationState memberState = connector.configure(order);
+            memberConfigurationStates.add(memberState);
+        }
+
+        OrderState nextOrderState = getNextOrderState(memberConfigurationStates);
+        OrderStateTransitioner.transition(order, nextOrderState);
+    }
+
+    private OrderState getNextOrderState(List<MemberConfigurationState> memberConfigurationStates) {
+        OrderState orderState = OrderState.FULFILLED;
+        boolean hasFail = false;
+        boolean hasSuccess = false;
+
+        for (MemberConfigurationState state : memberConfigurationStates) {
+            if (state == MemberConfigurationState.PARTIAL_SUCCESS) {
+                orderState = OrderState.PARTIALLY_FULFILLED;
+                break;
+            }  else if (state == MemberConfigurationState.SUCCESS) {
+                hasSuccess = true;
+            }  else {
+                hasFail = true;
             }
         }
+
+        if (orderState != OrderState.PARTIALLY_FULFILLED) {
+            if (hasFail) {
+                orderState = hasSuccess ? OrderState.PARTIALLY_FULFILLED : OrderState.FAILED;
+            }
+        }
+
+        return orderState;
     }
 }
