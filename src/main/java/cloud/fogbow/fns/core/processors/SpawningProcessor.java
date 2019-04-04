77f71f49a244708c8faa6143b30b33c062d9b3e6
Paulo Ewerton <pauloewerton@lsd.ufcg.edu.br>
2019-04-04 17:08:57 -0300

    Minor Refactoring

--- src/main/java/cloud/fogbow/fns/core/processors/SpawningProcessor.java
+++ src/main/java/cloud/fogbow/fns/core/processors/SpawningProcessor.java
@@ -2,29 +2,19 @@ package cloud.fogbow.fns.core.processors;
 
 import cloud.fogbow.common.exceptions.UnexpectedException;
 import cloud.fogbow.common.models.linkedlists.ChainedList;
-import cloud.fogbow.fns.constants.ApiDocumentation;
 import cloud.fogbow.fns.constants.Messages;
 import cloud.fogbow.fns.core.FederatedNetworkOrderController;
 import cloud.fogbow.fns.core.FederatedNetworkOrdersHolder;
 import cloud.fogbow.fns.core.OrderStateTransitioner;
-import cloud.fogbow.fns.core.exceptions.AgentCommucationException;
-import cloud.fogbow.fns.core.exceptions.InvalidCidrException;
 import cloud.fogbow.fns.core.model.ConfigurationMode;
 import cloud.fogbow.fns.core.model.FederatedNetworkOrder;
 import cloud.fogbow.fns.core.model.MemberConfigurationState;
 import cloud.fogbow.fns.core.model.OrderState;
 import cloud.fogbow.fns.core.serviceconnector.ServiceConnector;
 import cloud.fogbow.fns.core.serviceconnector.ServiceConnectorFactory;
-import cloud.fogbow.fns.utils.AgentCommunicatorUtil;
-import cloud.fogbow.fns.utils.FederatedNetworkUtil;
-import cloud.fogbow.ras.core.models.orders.Order;
-import org.apache.commons.net.util.SubnetUtils;
 import org.apache.log4j.Logger;
-import org.aspectj.weaver.ast.Or;
 
-import javax.xml.ws.Service;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -66,20 +56,14 @@ public class SpawningProcessor implements Runnable {
             // Check if the order is still SPAWNING (its state may have been changed by another thread)
             if (order.getOrderState().equals(OrderState.SPAWNING)) {
                 ConfigurationMode configurationMode = order.getConfigurationMode();
-                ServiceConnector connector = ServiceConnectorFactory.getInstance().getServiceConnector(
-                        order.getConfigurationMode(), order.getRequestingMember());
 
                 switch(configurationMode) {
                     case VANILLA:
-                        MemberConfigurationState state = connector.configure(order);
-                        if (state == MemberConfigurationState.SUCCESS) {
-                            OrderStateTransitioner.transition(order, OrderState.FULFILLED);
-                        } else {
-                            OrderStateTransitioner.transition(order, OrderState.FAILED);
-                        }
+                        this.processVanillaOrder(order);
                         break;
                     case DFNS:
-                        this.createDFNSFederatedNetwork(order);
+                        this.processDFNSOrder(order);
+                        break;
                     default:
                         throw new RuntimeException(Messages.Exception.CONFIGURATION_MODE_NOT_IMPLEMENTED);
                 }
@@ -89,7 +73,19 @@ public class SpawningProcessor implements Runnable {
         }
     }
 
-    private void createDFNSFederatedNetwork(FederatedNetworkOrder order) throws UnexpectedException {
+    private void processVanillaOrder(FederatedNetworkOrder order) throws UnexpectedException {
+        ServiceConnector connector = ServiceConnectorFactory.getInstance().getServiceConnector(
+                ConfigurationMode.VANILLA, order.getRequestingMember());
+        MemberConfigurationState state = connector.configure(order);
+
+        if (state == MemberConfigurationState.SUCCESS) {
+            OrderStateTransitioner.transition(order, OrderState.FULFILLED);
+        } else {
+            OrderStateTransitioner.transition(order, OrderState.FAILED);
+        }
+    }
+
+    private void processDFNSOrder(FederatedNetworkOrder order) throws UnexpectedException {
         List<MemberConfigurationState> memberConfigurationStates = new ArrayList<>();
         for (Map.Entry<String, MemberConfigurationState> provider : order.getProviders().entrySet()) {
             ServiceConnector connector = ServiceConnectorFactory.getInstance().getServiceConnector(
