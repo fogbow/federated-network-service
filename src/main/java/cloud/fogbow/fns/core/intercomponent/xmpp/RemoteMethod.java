0e5c4ba105a949bede9fad26ffad91fd771890f3
Paulo Ewerton <pauloewerton@lsd.ufcg.edu.br>
2019-04-18 17:37:46 -0300

    Adding remote remove fednet request and handler

--- src/main/java/cloud/fogbow/fns/core/intercomponent/xmpp/RemoteMethod.java
+++ src/main/java/cloud/fogbow/fns/core/intercomponent/xmpp/RemoteMethod.java
@@ -3,7 +3,8 @@ package cloud.fogbow.fns.core.intercomponent.xmpp;
 public enum RemoteMethod {
     REMOTE_CONFIGURE_MEMBER("remoteConfigureMember"),
     REMOTE_GET_FREE_VLAN_ID("getFreeVlanId"),
-    REMOTE_RELEASE_VLAN_ID("releaseVlanId");
+    REMOTE_RELEASE_VLAN_ID("releaseVlanId"),
+    REMOTE_REMOVE_FEDNET("removeFederatedNetwork");
 
     private final String namespace;
 
