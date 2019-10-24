981ac4be42a0a0345f5a3effbb0b3bc4e5dc70ad
raonismaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-10-24 14:43:24 -0300

    removing useless import

--- src/main/java/cloud/fogbow/fns/core/drivers/vanilla/VanillaServiceDriver.java
+++ src/main/java/cloud/fogbow/fns/core/drivers/vanilla/VanillaServiceDriver.java
@@ -25,7 +25,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.UUID;
 
