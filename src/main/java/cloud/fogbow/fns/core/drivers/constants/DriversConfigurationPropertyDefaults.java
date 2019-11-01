8157460bdee62527106f3209ba514165455401d0
fubica <fubica@dsc.ufcg.edu.br>
2019-11-01 16:14:36 -0300

    Added a default value for AGENT_SCRIPTS_PATH

---
+++ src/main/java/cloud/fogbow/fns/core/drivers/constants/DriversConfigurationPropertyDefaults.java
@@ -0,0 +1,5 @@
+package cloud.fogbow.fns.core.drivers.constants;
+
+public class DriversConfigurationPropertyDefaults {
+    public static final String AGENT_SCRIPTS_PATH = "/home/ubuntu/fogbow-components/federated-network-agent/";
+}
