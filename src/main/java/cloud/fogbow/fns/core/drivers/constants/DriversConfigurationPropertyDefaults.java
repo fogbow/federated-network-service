c1bd4bcbf43ac74358d13ffedd1f0ff6103af54d
fubica <fubica@dsc.ufcg.edu.br>
2019-11-05 06:56:25 -0300

    Fixes

--- src/main/java/cloud/fogbow/fns/core/drivers/constants/DriversConfigurationPropertyDefaults.java
+++ src/main/java/cloud/fogbow/fns/core/drivers/constants/DriversConfigurationPropertyDefaults.java
@@ -1,5 +1,5 @@
 package cloud.fogbow.fns.core.drivers.constants;
 
 public class DriversConfigurationPropertyDefaults {
-    public static final String AGENT_SCRIPTS_PATH = "~/fogbow-components/federated-network-agent/";
+    public static final String AGENT_SCRIPTS_PATH = "fogbow-components/federated-network-agent/";
 }
